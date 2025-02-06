f558832f93d0aa960928f8c021aee313c9b2a6ef
RupendraCHA <chandaluri210@gmail.com>
2025-02-07 02:13:48 +0530

    React Hooks

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+import UseState from './Hooks/use-state/useState'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <UseState/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Hooks/use-state/UseState.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+const UseState = () => {
+
+    const [count, setCount] = useState(0)
+
+    const increaseCount = () => {
+        setCount((prev) => prev+1)
+    }
+  return (
+    <div>
+        <h1 className='font-bold text-6xl mb-6'>Count: {count}</h1>
+        <button className='bg-white text-black px-2 font-bold py-1 rounded-sm' onClick={increaseCount}>
+            Increase Count
+        </button>
+    </div>
+  )
+}
+
+export default UseState
